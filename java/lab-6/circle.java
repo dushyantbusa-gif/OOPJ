3d82fea5f226640d9c8783b44e906962ddc6950b
dushyantbusa-gif <dusyantbusa@gmail.com>
2026-06-24 18:22:38 +0530

    Add files via upload

---
+++ java/lab-6/circle.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class circle{
+	public static void main(String[] args){
+	 Scanner sc = new Scanner(System.in);
+		float r = sc.nextFloat();
+		float pi = 3.14f;
+		float circle=pi*(r*r);
+		System.out.println("circle is "+circle);
+}
+}
